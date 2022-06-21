68d6d060f82dabbcd5082eca712a1b77be5471ae
Cavid Bashirov <bashirovcavid93gmail.com>
2022-06-21 13:12:45 +0400

    Explained string

--- String-Array/Program.cs
+++ String-Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace String_Array
 {
@@ -6,7 +7,127 @@ namespace String_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int a = 5;
+            //int b = a;
+
+            // Console.WriteLine(a + "-" + b);
+
+            //int[] arr1 = { 1, 2, 3, 5, 7 };  
+            //int[] arr2 = arr1;
+            //arr2[0] = 100;
+
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+
+            //int a;
+            //CheckNum(out a);
+            //Console.WriteLine(a);
+
+            //string name1 = "Orxan";
+            //string name2 = name1;
+            //name2 = "Emil";
+
+            //Console.WriteLine(name1 + "-" +name2);
+
+            //string name1 = "Or";
+            //string name2 = "xan";
+            //string fullName = name1 + name2;
+            //Console.WriteLine(fullName);
+
+
+
+
+            //StringBuilder name = new StringBuilder();
+
+            //name.Append("Or");
+            //name.Append("xan");
+
+            //Console.WriteLine(name);
+
+
+
+            string name = "Mirsamir";
+
+
+            // name = string.Empty;
+
+
+
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    Console.WriteLine("Dogrudur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sehvdir");
+            //}
+
+            //if (name.StartsWith("Mi"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+
+            //if (name.EndsWith("r"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+            //Console.WriteLine(name.Length);
+
+            //Console.WriteLine(name.LastIndexOf("s"));
+
+            // var res = name.Replace("m", "b");
+
+            //Console.WriteLine(name.Substring(3));
+            //Console.WriteLine(name.Trim());
+
+            //string sentence = "Hello P130 , how are you ? , bye bye";
+
+            //var result = sentence.Split(",");
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //foreach (var item in name)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine(name.ToUpper());
+
+            //if (name.ToLower().StartsWith("m"))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+
+
+
         }
+
+
+        //static void CheckNum(out int num)
+        //{
+        //    num = 100;
+
+        //    Console.WriteLine($"Number : {num}");
+        //}
     }
 }
